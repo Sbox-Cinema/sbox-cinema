26d7faf0d217b85d74c3d1bebf77558805980e56
Zachary <remscar@gmail.com>
2023-04-20 11:23:10 -0700

    Added imports

--- code/entities/projector/Projection.cs
+++ code/entities/projector/Projection.cs
@@ -1,63 +1,65 @@
-﻿namespace Cinema;
+﻿using Sandbox;
+
+namespace Cinema;
 
 public partial class Projection : Entity
 {
-	public ProjectorEntity ProjectorEntity { get; set; }
-	public WebMediaSource MediaSource { get; set; }
-	private SceneWorld ProjectorSceneWorld { get; set; }
-	private SceneCamera ProjectorSceneCamera { get; set; }
-	private Texture ProjectionTexture { get; set; }
-	private OrthoLightEntity ProjectionLight { get; set; }
+    public ProjectorEntity ProjectorEntity { get; set; }
+    public WebMediaSource MediaSource { get; set; }
+    private SceneWorld ProjectorSceneWorld { get; set; }
+    private SceneCamera ProjectorSceneCamera { get; set; }
+    private Texture ProjectionTexture { get; set; }
+    private OrthoLightEntity ProjectionLight { get; set; }
 
-	public Projection(ProjectorEntity ent, WebMediaSource mediaSrc)
-	{
-		ProjectorEntity = ent;
-		MediaSource = mediaSrc;
+    public Projection(ProjectorEntity ent, WebMediaSource mediaSrc)
+    {
+        ProjectorEntity = ent;
+        MediaSource = mediaSrc;
 
-		InitProjectionScene();
-		InitProjectionImage();
-	}
-	private void InitProjectionScene()
-	{
-		//Initialize Scene World
-		ProjectorSceneWorld = new SceneWorld();
+        InitProjectionScene();
+        InitProjectionImage();
+    }
+    private void InitProjectionScene()
+    {
+        //Initialize Scene World
+        ProjectorSceneWorld = new SceneWorld();
 
-		//Initialize Scene Camera
-		ProjectorSceneCamera = new SceneCamera();
-		ProjectorSceneCamera.World = ProjectorSceneWorld;
-		ProjectorSceneCamera.Position = new Vector3();
-		ProjectorSceneCamera.ZNear = 1.0f;
-		ProjectorSceneCamera.ZFar = 15000.0f;
+        //Initialize Scene Camera
+        ProjectorSceneCamera = new SceneCamera();
+        ProjectorSceneCamera.World = ProjectorSceneWorld;
+        ProjectorSceneCamera.Position = new Vector3();
+        ProjectorSceneCamera.ZNear = 1.0f;
+        ProjectorSceneCamera.ZFar = 15000.0f;
 
-		//Initialize Texture
-		ProjectionTexture = Texture.CreateRenderTarget( "projection-img", ImageFormat.RGBA8888, ProjectorEntity.ProjectionResolution );
+        //Initialize Texture
+        ProjectionTexture = Texture.CreateRenderTarget("projection-img", ImageFormat.RGBA8888, ProjectorEntity.ProjectionResolution);
 
-		//Initialize Media Panel
-		MediaSource = new WebMediaSource( ProjectorEntity, ProjectorSceneWorld );
-		MediaSource.Position = ProjectorSceneCamera.Position + (Vector3.Forward * 36.0f);
-		MediaSource.Rotation = Rotation.FromYaw( 180 );
-	}
-	private void InitProjectionImage()
-	{
-		ProjectionLight = new OrthoLightEntity
-		{
-			Parent = ProjectorEntity,
-			Position = ProjectorEntity.Position,
-			Rotation = Rotation.From( ProjectorEntity.Rotation.Angles() ),
-			LightCookie = ProjectionTexture,
-			Brightness = 1.0f,
-			Range = 1024.0f,
-			OrthoLightWidth = ProjectorEntity.ProjectionSize.x,
-			OrthoLightHeight = ProjectorEntity.ProjectionSize.y,
-			DynamicShadows = true,
-		};
+        //Initialize Media Panel
+        MediaSource = new WebMediaSource(ProjectorEntity, ProjectorSceneWorld);
+        MediaSource.Position = ProjectorSceneCamera.Position + (Vector3.Forward * 36.0f);
+        MediaSource.Rotation = Rotation.FromYaw(180);
+    }
+    private void InitProjectionImage()
+    {
+        ProjectionLight = new OrthoLightEntity
+        {
+            Parent = ProjectorEntity,
+            Position = ProjectorEntity.Position,
+            Rotation = Rotation.From(ProjectorEntity.Rotation.Angles()),
+            LightCookie = ProjectionTexture,
+            Brightness = 1.0f,
+            Range = 1024.0f,
+            OrthoLightWidth = ProjectorEntity.ProjectionSize.x,
+            OrthoLightHeight = ProjectorEntity.ProjectionSize.y,
+            DynamicShadows = true,
+        };
 
-		ProjectionLight.UseFog();
-	}
+        ProjectionLight.UseFog();
+    }
 
-	[Event.PreRender]
-	private void RenderScene()
-	{
-		Graphics.RenderToTexture( ProjectorSceneCamera, ProjectionTexture );
-	}
+    [Event.PreRender]
+    private void RenderScene()
+    {
+        Graphics.RenderToTexture(ProjectorSceneCamera, ProjectionTexture);
+    }
 }
--- code/entities/projector/WebMediaSource.cs
+++ code/entities/projector/WebMediaSource.cs
@@ -1,81 +1,82 @@
-﻿using Sandbox.UI;
+﻿using Sandbox;
+using Sandbox.UI;
 using System;
 
 namespace Cinema;
 
 public partial class WebMediaSource : WorldPanel
 {
-	private ProjectorEntity Projector;
+    private ProjectorEntity Projector;
 
-	private WebSurface WebSurface;
-	public Texture WebTexture;
+    private WebSurface WebSurface;
+    public Texture WebTexture;
 
-	public WebMediaSource( ProjectorEntity ent )
-	{
-		Projector = ent;
+    public WebMediaSource(ProjectorEntity ent)
+    {
+        Projector = ent;
 
-		InitWorldPanel();
-		InitWebSurface();
-	}
-	public WebMediaSource(ProjectorEntity ent, SceneWorld world = null) : base( world )
-	{
-		Projector = ent;
-		
-		InitWorldPanel();
-		InitWebSurface();
-	}
-	private void InitWorldPanel()
-	{	
-		PanelBounds = new Rect( -Projector.ProjectionResolution.x / 2, -Projector.ProjectionResolution.y / 2, Projector.ProjectionResolution.x, Projector.ProjectionResolution.y );
-	}
-	private void InitWebSurface()
-	{
-		WebSurface = Game.CreateWebSurface();
-		WebSurface.Size = Projector.ProjectionResolution;
-		WebSurface.InBackgroundMode = false;
-		WebSurface.OnTexture = OnBrowserDataChanged;
+        InitWorldPanel();
+        InitWebSurface();
+    }
+    public WebMediaSource(ProjectorEntity ent, SceneWorld world = null) : base(world)
+    {
+        Projector = ent;
 
-		WebSurface.Url = "https://www.youtube.com/embed/XkfmrXLxaNk?autoplay=0;frameborder=0";
-	}
-	void ResetWebPanel()
-	{
-		WebSurface.Url = "https://www.youtube.com/embed/XkfmrXLxaNk?autoplay=1;frameborder=0";
-	}
-	public override void OnHotloaded()
-	{
-		ResetWebPanel();
-	}
-	private void UpdateWebTexture( ReadOnlySpan<byte> span, Vector2 size )
-	{
-		if ( WebTexture == null || WebTexture.Size != size )
-		{
-			WebTexture?.Dispose();
-			WebTexture = null;
-			WebTexture = Texture.Create( (int)size.x, (int)size.y, ImageFormat.BGRA8888 )
-				.WithDynamicUsage()
-				.WithName( "Web" )
-				.Finish();
+        InitWorldPanel();
+        InitWebSurface();
+    }
+    private void InitWorldPanel()
+    {
+        PanelBounds = new Rect(-Projector.ProjectionResolution.x / 2, -Projector.ProjectionResolution.y / 2, Projector.ProjectionResolution.x, Projector.ProjectionResolution.y);
+    }
+    private void InitWebSurface()
+    {
+        WebSurface = Game.CreateWebSurface();
+        WebSurface.Size = Projector.ProjectionResolution;
+        WebSurface.InBackgroundMode = false;
+        WebSurface.OnTexture = OnBrowserDataChanged;
 
-			Style.SetBackgroundImage( WebTexture );
-		}
-		WebTexture.Update( span, 0, 0, (int)size.x, (int)size.y );
-	}
-	void OnBrowserDataChanged(ReadOnlySpan<byte> span, Vector2 size)
-	{
-		if(Game.IsServer)
-		{
-			return;
-		}
+        WebSurface.Url = "https://www.youtube.com/embed/XkfmrXLxaNk?autoplay=0;frameborder=0";
+    }
+    void ResetWebPanel()
+    {
+        WebSurface.Url = "https://www.youtube.com/embed/XkfmrXLxaNk?autoplay=1;frameborder=0";
+    }
+    public override void OnHotloaded()
+    {
+        ResetWebPanel();
+    }
+    private void UpdateWebTexture(ReadOnlySpan<byte> span, Vector2 size)
+    {
+        if ( WebTexture == null || WebTexture.Size != size )
+        {
+            WebTexture?.Dispose();
+            WebTexture = null;
+            WebTexture = Texture.Create((int)size.x, (int)size.y, ImageFormat.BGRA8888)
+                .WithDynamicUsage()
+                .WithName("Web")
+                .Finish();
 
-		UpdateWebTexture( span, size );
-	}
-	public override void OnDeleted()
-	{
-		base.OnDeleted();
+            Style.SetBackgroundImage(WebTexture);
+        }
+        WebTexture.Update(span, 0, 0, (int)size.x, (int)size.y);
+    }
+    void OnBrowserDataChanged(ReadOnlySpan<byte> span, Vector2 size)
+    {
+        if ( Game.IsServer )
+        {
+            return;
+        }
 
-		WebTexture?.Dispose();
-		WebTexture = null;
+        UpdateWebTexture(span, size);
+    }
+    public override void OnDeleted()
+    {
+        base.OnDeleted();
 
-		WebSurface?.Dispose();
-	}
+        WebTexture?.Dispose();
+        WebTexture = null;
+
+        WebSurface?.Dispose();
+    }
 }
