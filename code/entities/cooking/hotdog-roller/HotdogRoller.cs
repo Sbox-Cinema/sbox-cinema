eef3501ce81db8ac4d9289d38b14ea0b2ed3160f
walker407 <hscott407@gmail.com>
2023-05-17 17:49:25 -0500

    Updating hammer entity icon...

--- code/entities/cooking/hotdog-roller/HotdogRoller.cs
+++ code/entities/cooking/hotdog-roller/HotdogRoller.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 namespace Cinema;
 
 [Library("ent_hotdog_roller"), HammerEntity]
-[Title("Hotdog Roller"), Category("Gameplay"), Icon("multicooker")]
+[Title("Hotdog Roller"), Category("Gameplay"), Icon("microwave")]
 [EditorModel("models/hotdogroller/hotdogroller.vmdl")]
 public partial class HotdogRoller : AnimatedEntity, ICinemaUse
 {
