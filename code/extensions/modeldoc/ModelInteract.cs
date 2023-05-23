1387be35b5bd1f13836a758235da463b9accf652
walker407 <hscott407@gmail.com>
2023-05-23 11:40:48 -0500

    Adding modeldoc/entity extensions

---
+++ code/extensions/modeldoc/ModelInteract.cs
@@ -0,0 +1,16 @@
+﻿using Sandbox;
+using Sandbox.ModelEditor;
+using System.Text.Json.Serialization;
+
+[GameData("Interactable Area", AllowMultiple = true)]
+[Box(dimensionsKey: nameof(size), Angles = nameof(rotation), Origin = nameof(origin))]
+public class ModelInteract
+{
+    public Vector3 size { get; set; }
+    public Vector3 origin { get; set; }
+    public Vector3 rotation { get; set; }
+
+    [JsonPropertyName("attachment_point"), FGDType("model_attachment")]
+    public string AttachmentPoint { get; set; }
+}
+
