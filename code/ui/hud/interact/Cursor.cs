2d3e91b7e499a8689490cf491decfab2621f4e3f
walker407 <hscott407@gmail.com>
2023-05-27 10:04:49 -0500

    Adding test cursor...

---
+++ code/ui/hud/interact/Cursor.cs
@@ -0,0 +1,12 @@
+﻿namespace Cinema.UI;
+public partial class Cursor
+{
+    private bool Visible { get; set; } = true;
+ 
+    public void ShouldOpen(bool open)
+    {
+        Visible = open;
+
+        StateHasChanged();
+    }
+}
