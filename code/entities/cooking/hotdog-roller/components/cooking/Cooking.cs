0b7b0011664d8373e4be28ad70085ca4bb0c2389
walker407 <hscott407@gmail.com>
2023-06-02 11:13:27 -0500

    Simplifying cooking entity component...

--- code/entities/cooking/hotdog-roller/components/cooking/Cooking.cs
+++ code/entities/cooking/hotdog-roller/components/cooking/Cooking.cs
@@ -3,40 +3,35 @@
 namespace Cinema;
 public partial class Cooking : EntityComponent
 {
-    private TimeSince SinceCooking { get; set; }
-
     private float TimeToCook = 8.0f;
+    private TimeSince SinceCooking { get; set; }
+    private bool DoneCooking => SinceCooking > TimeToCook;
+    private bool IsCooked { get; set; } = false;
 
     /// <summary>
     /// Called when activated
-    /// Sets initial state
     /// </summary>
     protected override void OnActivate()
     {
         base.OnActivate();
 
-        SetInitState();
-    }
-
-    /// <summary>
-    /// Sets initial state default (Raw)
-    /// </summary>
-    private void SetInitState()
-    {
         SinceCooking = 0.0f;
-
-        TransitionStateTo(CookState.Raw);
     }
 
     /// <summary>
-    /// Checks for raw to cooked state transition
+    /// 
     /// </summary>
     [GameEvent.Tick]
     private void OnTick()
     {
-        if (SinceCooking > TimeToCook && State is CookState.Raw)
+        if (DoneCooking)
         {
-            TransitionStateTo(CookState.Cooked);
+            if (!IsCooked)
+            {
+                Entity.Components.GetOrCreate<Steam>();
+
+                IsCooked = true;
+            }
         } 
     }
 }
