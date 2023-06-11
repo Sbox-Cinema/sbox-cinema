90e791b6abdeeddc5fc8d42330699e2e1cd021e5
ducc <spielerc@gmail.com>
2023-06-11 17:32:44 -0600

    Started work on video player

---
+++ code/mediacontrol/IVideoProvider.cs
@@ -0,0 +1,16 @@
+﻿using Sandbox;
+using Sandbox.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema;
+
+public interface IVideoProvider
+{
+    public string ProviderName { get; }
+    public Panel SelectorPanel { get; }
+    public void PlayVideo(string )
+}
---
+++ code/mediacontrol/VideoProviderContext.cs
@@ -0,0 +1,14 @@
+﻿using Sandbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema;
+
+public class VideoProviderContext
+{
+    public string Url { get; }
+    public Texture TargetTexture { get; }
+}
