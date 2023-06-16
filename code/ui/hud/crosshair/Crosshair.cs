1bbab80d149e883b94887cb0e8c8625e5d9300a7
Oliver <38598074+Stromic@users.noreply.github.com>
2023-06-16 16:59:37 +0200

    Super basic crosshair
    This is just the start, but will be interactive in the future

---
+++ code/ui/hud/crosshair/Crosshair.cs
@@ -0,0 +1,15 @@
+﻿using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+using System.Text;
+
+namespace Cinema.UI;
+
+public partial class Crosshair : Panel
+{
+    public Crosshair()
+    {
+        StyleSheet.Load("/ui/hud/crosshair/Crosshair.scss");
+        Log.Info("Workin");
+    }
+}
