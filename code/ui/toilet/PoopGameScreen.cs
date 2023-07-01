1fdac76bc16b1aa53d89d77086042c83ad17984b
Zachary <remscar@gmail.com>
2023-07-01 00:41:53 -0700

    Square guts

--- code/ui/toilet/PoopGameScreen.cs
+++ code/ui/toilet/PoopGameScreen.cs
@@ -23,7 +23,7 @@ public partial class PoopGameScreen : Panel
 
     public override void Tick()
     {
-        PoopPosition = PoopPosition.WithX(0);
+        //PoopPosition = PoopPosition.WithX(0);
 
         if (Input.Down("jump"))
         {
@@ -58,57 +58,30 @@ public partial class PoopGameScreen : Panel
         {
             PoopVelocity = PoopVelocity.WithY(0);
         }
-    }
 
-    public Vector2[] BottomTunnelVertices { get; set; } = Array.Empty<Vector2>();
-
-    [Event.Hotload]
-    public override void OnHotloaded()
-    {
-        Log.Info("Hotloading PoopGameScreen");
-
-        var waves = 2;
-        var granularity = 0.1f;
-        var scale = PlayArea.x / waves;
-        var height = 40f;
-
-        var vertices = new List<Vector2>();
-
-        var lastX = 0f;
-        var lastY = height;
-
-        vertices.Add(new Vector2(lastX, 0));
-        vertices.Add(new Vector2(lastX, lastY));
-        vertices.Add(new Vector2(lastX + granularity * scale, 0));
-
-        for (var x = granularity; x < waves; x += granularity)
+        PoopPosition += new Vector2(200, 0) * Time.Delta;
+        if (PoopPosition.x + PoopSize.x >= PlayArea.x)
         {
-            var y = (float)Math.Sin(x * Math.PI * 2) * height + height;
-
-            var realX = x * scale;
-            vertices.Add(new Vector2(lastX, lastY));
-            vertices.Add(new Vector2(realX, y));
-            vertices.Add(new Vector2(realX, 0));
-
-            vertices.Add(new Vector2(lastX, 0));
-            vertices.Add(new Vector2(lastX, lastY));
-            vertices.Add(new Vector2(lastX + granularity * scale, 0));
-
-            lastX = realX;
-            lastY = y;
+            PoopPosition = new Vector2(0, PlayArea.y - PoopSize.y);
+            PoopVelocity = Vector2.Zero;
         }
-
-        BottomTunnelVertices = vertices.ToArray();
     }
 
     public override void DrawContent(ref RenderState state)
     {
-        //DrawRectangle(Vector2.Zero, PlayArea, new Color(0.35f, 0.15f, 0.18f));
-        DrawRectangle(PoopPosition, PoopSize, Color.FromRgb(0xffffff));
-        DrawVertices(BottomTunnelVertices, Color.FromRgb(0xffffff));
+        DrawRectangle(
+            new Vector2(0, PlayArea.y - 300),
+            new Vector2(300, 300),
+            new Color(0.35f, 0.15f, 0.18f)
+        );
+        DrawRectangle(new Vector2(100, 20), new Vector2(600, 200), new Color(0.35f, 0.15f, 0.18f));
+        DrawRectangle(new Vector2(500, 200), new Vector2(800, 200), new Color(0.35f, 0.15f, 0.18f));
+        DrawRectangle(new Vector2(1100, 50), new Vector2(500, 200), new Color(0.35f, 0.15f, 0.18f));
+
+        DrawRectangle(PoopPosition, PoopSize, Color.FromRgb(0xffffff), "textures/poopemoji.png");
     }
 
-    private void DrawRectangle(Vector2 pos, Vector2 size, Color color)
+    private void DrawRectangle(Vector2 pos, Vector2 size, Color color, string texture = null)
     {
         var aspectRatio = PlayArea.x / PlayArea.y;
         var drawArea = new Vector2(Box.Rect.Size.y * aspectRatio, Box.Rect.Size.y);
@@ -127,44 +100,15 @@ public partial class PoopGameScreen : Panel
         var drawSpaceSize = drawArea;
         var actualPosition = drawSpaceSize * relativePosition;
         var actualSize = drawSpaceSize * relativeSize;
-        //Log.Info($"actualPosition: {actualPosition} actualSize: {actualSize}");
-
-
 
         var x = Box.Rect.TopLeft.x + actualPosition.x + margin.x / 2;
         var y = Box.Rect.BottomLeft.y - actualPosition.y - actualSize.y - margin.y / 2;
         Util.Draw.Reset();
         Util.Draw.Color = color;
-        var poop = Texture.Load(FileSystem.Mounted, "textures/poopemoji.png");
-        Util.Draw.Texture = poop;
+        if (texture is not null)
+        {
+            Util.Draw.Texture = Texture.Load(FileSystem.Mounted, texture);
+        }
         Util.Draw.Quad(new Rect(x, y, actualSize.x, actualSize.y));
-    }
-
-    private void DrawVertices(Vector2[] vertices, Color color)
-    {
-        var aspectRatio = PlayArea.x / PlayArea.y;
-        var drawArea = new Vector2(Box.Rect.Size.y * aspectRatio, Box.Rect.Size.y);
-
-        if (drawArea.x > Box.Rect.Size.x)
-        {
-            drawArea = new Vector2(Box.Rect.Size.x, Box.Rect.Size.x / aspectRatio);
-        }
-
-        var margin = Box.Rect.Size - drawArea;
-        var drawSpaceSize = drawArea;
-
-        Util.Draw.Reset();
-        Util.Draw.Color = color;
-
-        Util.Draw.MeshStart();
-        foreach (var vert in vertices)
-        {
-            var relativePosition = vert / PlayArea;
-            var actualPosition = drawSpaceSize * relativePosition;
-            var x = Box.Rect.TopLeft.x + actualPosition.x + margin.x / 2;
-            var y = Box.Rect.BottomLeft.y - actualPosition.y - margin.y / 2;
-            Util.Draw.AddVertex(new Vector2(x, y), color);
-        }
-        Util.Draw.MeshEnd();
     }
 }
