88c198dad10c0babec0267d60facabf9b1d2740c
ducc <spielerc@gmail.com>
2023-07-13 07:03:08 -0600

    Add browser history, work on vote ordering algo

--- code/mediacontrol/MediaQueue.cs
+++ code/mediacontrol/MediaQueue.cs
@@ -9,47 +9,17 @@ namespace Cinema;
 
 public partial class MediaQueue : EntityComponent<CinemaZone>, ISingletonComponent
 {
-    public partial class ScoredItem : BaseNetworkable, IComparable<ScoredItem>
+    public partial class ScoredItem : BaseNetworkable
     {
         [Net]
         public MediaRequest Item { get; set; }
         [Net]
-        public int Id { get; set; }
-        [Net]
-        public int PriorityOffset { get; set; }
-        [Net]
         public IDictionary<IClient, bool> PriorityVotes { get; set; }
-
-        public void Vote(IClient client, bool isUpvote)
-        {
-            if (PriorityVotes.TryGetValue(client, out var existingVote))
-            {
-                if (existingVote == isUpvote)
-                {
-                    return;
-                }
-                PriorityVotes[client] = isUpvote;
-                PriorityOffset += isUpvote ? 1 : -1;
-            }
-            else
-            {
-                PriorityVotes.Add(client, isUpvote);
-                PriorityOffset += isUpvote ? 1 : -1;
-            }
-        }
-
-        public int CompareTo(ScoredItem other)
-        {
-            return (PriorityOffset + Id).CompareTo(other.PriorityOffset + other.Id);
-        }
     }
 
     [Net]
     public IList<ScoredItem> Items { get; set; }
     public MediaController Controller => Entity.MediaController;
-    public IEnumerable<ScoredItem> GetAll() => Items.OrderBy(i => i);
-
-    private static int NextId { get; set; } = 0;
 
 
     [GameEvent.Tick.Server]
@@ -68,21 +38,21 @@ public partial class MediaQueue : EntityComponent<CinemaZone>, ISingletonCompone
         }
     }
 
+    private static MediaQueue FindByZoneId(int zoneId)
+    {
+        var zone = Sandbox.Entity.FindByIndex(zoneId) as CinemaZone;
+        return zone?.MediaQueue;
+    }
+
     public MediaRequest Pop()
     {
-        var item = Items.OrderBy(i => i).FirstOrDefault();
+        var item = Items.FirstOrDefault();
 
         if (item == null)
             return null;
 
         Items.Remove(item);
         return item.Item;
-    }
-
-    private static MediaQueue FindByZoneId(int zoneId)
-    {
-        var zone = Sandbox.Entity.FindByIndex(zoneId) as CinemaZone;
-        return zone?.MediaQueue;
     }
 
     [ConCmd.Server]
@@ -97,6 +67,67 @@ public partial class MediaQueue : EntityComponent<CinemaZone>, ISingletonCompone
 
     public void Push(MediaRequest request)
     {
-        Items.Add(new ScoredItem { Item = request, Id = NextId++ });
+        Items.Add(new ScoredItem { Item = request });
+    }
+
+    public void AddPriorityVote(MediaRequest request, IClient client, bool isUpvote)
+    {
+        var item = Items.FirstOrDefault(i => i.Item == request);
+        if (item == null)
+            return;
+
+        // If this client already voted, don't 
+        if (item.PriorityVotes.ContainsKey(client) && item.PriorityVotes[client] == isUpvote)
+            return;
+        item.PriorityVotes[client] = isUpvote;
+        var index = Items.IndexOf(item);
+        // If we remove a true vote:
+        // - Index goes down
+        // - Priority goes up
+        SwapItem(index, isUpvote ? -1 : 1);
+    }
+
+    public void RemovePriorityVote(MediaRequest request, IClient client)
+    {
+        var item = Items.FirstOrDefault(i => i.Item == request);
+        // If no priority votes were cast, there's nothing to remove. 
+        if (item != null && !item.PriorityVotes.ContainsKey(client))
+            return;
+
+        var voteValue = item.PriorityVotes[client];
+        var index = Items.IndexOf(item);
+        // If we remove a true vote:
+        // - Index goes down
+        // - Priority goes up
+        SwapItem(index, voteValue ? -1 : 1);
+    }
+
+    /// <summary>
+    /// Change index (and inversely, the priority) of queued media by the
+    /// specified amount. Swaps the item at the specified index with
+    /// adjacent items until it has been swapped by the desired amount.
+    /// </summary>
+    /// <param name="index">The index of the item to swap.</param>
+    /// <param name="offset">The amount of places by which this item should be swapped.</param>
+    private void SwapItem(int index, int offset)
+    {
+        if (index < 0 || index >= Items.Count)
+        {
+            Log.Error($"Invalid media queue index: {index}");
+            return;
+        }
+        var offsetDirection = Math.Sign(offset);
+        // Swap the specified item with the adjacent item in the direction
+        // of the offset. Repeat until we've arrived at the desired offset.
+        for (int i = 0; i != offset; i += offsetDirection)
+        {
+            // Find the index of the adjacent item we will swap with.
+            var swapIndex = index + i * offsetDirection;
+            // If we've reached the end of the queue, no need to swap further.
+            if (swapIndex < 0 || swapIndex >= Items.Count)
+                return;
+            // Swap the items.
+            (Items[swapIndex], Items[index]) = (Items[index], Items[swapIndex]);
+        }
     }
 }
--- plugins/interfaces/video/BrowserMediaProviderPanel.razor.cs
+++ plugins/interfaces/video/BrowserMediaProviderPanel.razor.cs
@@ -1,4 +1,7 @@
 ﻿using Sandbox.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CinemaTeam.Plugins.Video;
 
@@ -8,7 +11,10 @@ public partial class BrowserMediaProviderPanel : MediaProviderHeaderPanel
     public Panel BrowserPanel { get; set; }
     public string DefaultUrl { get; set; } = "https://www.google.com";
 
-    private string PreviousUrl { get; set; }
+    protected List<string> BrowserHistory { get; set; } = new();
+    protected int BrowserHistoryIndex { get; set; } = -1;
+
+    private string LastKnownWebSurfaceUrl { get; set; }
 
     protected override void OnAfterTreeRender(bool firstTime)
     {
@@ -16,23 +22,91 @@ public partial class BrowserMediaProviderPanel : MediaProviderHeaderPanel
             return;
 
         var browser = BrowserPanel as WebPanel;
-        browser.Surface.Url = "https://www.youtube.com";
+        browser.Surface.Url = DefaultUrl;
+        OnUrlChanged(null, DefaultUrl);
+    }
+
+    protected override int BuildHash()
+    {
+        return HashCode.Combine(BrowserHistory.Count, BrowserHistoryIndex);
     }
 
     public override void Tick()
     {
-        base.Tick();
-
         var browser = BrowserPanel as WebPanel;
-        if (browser.Surface.Url != PreviousUrl)
+        var currentUrl = browser.Surface.Url;
+        // If the web surface URL changed on its own
+        // (i.e. without clicking back or forward)
+        if (currentUrl != LastKnownWebSurfaceUrl)
         {
+            // When a WebSurface is created and first navigates to a URL,
+            // it may have a null, garbage, or equivalent URL. 
+            // We don't want to flood the browser history with these.
+            var newUrlIsNull = browser.Surface.Url == null;
+            var newUrlIsGarbage = browser.Surface.Url?.StartsWith("data:") ?? false;
+            var newUrlIsEquivalent = browser.Surface.Url == LastKnownWebSurfaceUrl + "/";
+            if (newUrlIsNull || newUrlIsGarbage || newUrlIsEquivalent)
+                return;
+
             var urlText = UrlTextEntry as TextEntry;
-            urlText.Text = browser?.Surface?.Url;
-            PreviousUrl = browser?.Surface?.Url;
+            urlText.Text = currentUrl;
+            OnUrlChanged(LastKnownWebSurfaceUrl, currentUrl);
         }
     }
 
-    public void OnClickAdd()
+    protected virtual void OnUrlChanged(string oldUrl, string newUrl)
+    {
+        if (newUrl == null)
+            return;
+
+        bool atEndOfHistory = BrowserHistoryIndex == BrowserHistory.Count - 1;
+        // If we are overwriting the future...
+        if (BrowserHistory.Any() && !atEndOfHistory)
+        {
+            // ...and the new url is not the next url in the history...
+            if (BrowserHistory[BrowserHistoryIndex + 1] != newUrl)
+            {
+                // ...then destroy the future.
+                BrowserHistory.RemoveRange(BrowserHistoryIndex + 1, BrowserHistory.Count - BrowserHistoryIndex - 1);
+            }
+        }
+        BrowserHistoryIndex++;
+        BrowserHistory.Add(newUrl);
+        LastKnownWebSurfaceUrl = newUrl;
+        Log.Trace($"Url changed, Browser URL: {(BrowserPanel as WebPanel).Surface.Url}, Old URL: {oldUrl}, New URL: {newUrl}, Browser history size: {BrowserHistory.Count}, Browser history index: {BrowserHistoryIndex}");
+    }
+
+    public virtual bool CanGoBack()
+        => BrowserHistory.Any() && BrowserHistoryIndex > 0;
+
+    public virtual bool CanGoForward()
+        => BrowserHistoryIndex < BrowserHistory.Count - 1;
+    
+    public virtual void OnClickBack()
+    {
+        if (!CanGoBack())
+            return;
+
+        BrowserHistoryIndex--;
+        var browser = BrowserPanel as WebPanel;
+        var previousUrl = BrowserHistory[BrowserHistoryIndex];
+        browser.Surface.Url = previousUrl;
+        LastKnownWebSurfaceUrl = previousUrl;
+    }
+
+    public virtual void OnClickForward()
+    {
+        if (!CanGoForward())
+            return;
+
+        BrowserHistoryIndex++;
+        var browser = BrowserPanel as WebPanel;
+        var nextUrl = BrowserHistory[BrowserHistoryIndex];
+        browser.Surface.Url = nextUrl;
+        LastKnownWebSurfaceUrl = nextUrl;
+    }
+
+    public virtual void OnClickAdd()
     {
         var urlText = UrlTextEntry as TextEntry;
         var url = urlText.Text;
