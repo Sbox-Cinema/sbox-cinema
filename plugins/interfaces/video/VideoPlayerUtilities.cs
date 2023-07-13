83cb2774d7057cc7ab95b392c6653bcb54cb02eb
ducc <spielerc@gmail.com>
2023-07-13 09:43:28 -0600

    Add default map, VideoPlayerUtilities

---
+++ plugins/interfaces/video/VideoPlayerUtilities.cs
@@ -0,0 +1,27 @@
+﻿using Sandbox;
+using System.Threading.Tasks;
+
+namespace CinemaTeam.Plugins.Video;
+
+public static class VideoPlayerUtilities
+{
+    /// <summary>
+    /// Retrieve the duration in seconds of a video from a URL.
+    /// </summary>
+    /// <param name="url">The URL of a video such as a .mp4 or .webm</param>
+    /// <returns>The video duration in seconds.</returns>
+    public async static Task<float> GetVideoDuration(string url)
+    {
+        var isLoaded = false;
+        var vid = new VideoPlayer();
+        vid.OnLoaded += () => isLoaded = true;
+        vid.Play(url);
+        while (!isLoaded)
+        {
+            await GameTask.DelaySeconds(Time.Delta);
+        }
+        var duration = vid.Duration;
+        vid.Dispose();
+        return duration;
+    }
+}
