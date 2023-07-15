5fdf6d259249b366677aa6e980cff49cbe9c8ffd
walker407 <hscott407@gmail.com>
2023-07-15 16:00:34 -0500

    Initial commit

---
+++ code/entities/machine/vending/SodaMachine.Use.cs
@@ -0,0 +1,56 @@
+﻿using Sandbox;
+
+namespace Cinema;
+
+public partial class SodaMachine
+{
+    public string UseText { get; set; } = "Use Soda Vending Machine";
+
+    /// <summary>
+    /// Whether this Soda Fountain is usable or not
+    /// </summary>
+    /// <param name="user">The player who is using</param>
+    /// <returns>If this is useable</returns>
+    public virtual bool IsUsable(Entity user)
+    {
+        return true;
+    }
+
+    /// <summary>
+    /// Called on the server when the Soda Fountain is used by a player
+    /// </summary>
+    /// <param name="user"></param>
+    /// <returns>If the player can continue to use the Hotdog Roller</returns>
+    public virtual bool OnUse(Entity user)
+    {
+        if (Game.IsClient) return false;
+
+        HandleUse(user);
+
+        return false;
+    }
+
+    /// <summary>
+    /// Called on the server when the Soda Fountain is stopped being used by a player
+    /// </summary>
+    /// <param name="user"></param>
+    /// <returns>If the player can continue to use the Hotdog Roller</returns>
+    public void OnStopUse(Entity user)
+    {
+
+    }
+
+    public void HandleUse(Entity player)
+    {
+        foreach (var (_, interactable) in Interactables)
+        {
+            var rayResult = interactable.CanRayTrigger(player.AimRay);
+
+            if (rayResult.Hit)
+            {
+                interactable.Trigger(player as Player);
+                break;
+            }
+        }
+    }
+}
---
+++ code/entities/machine/vending/SodaMachine.cs
@@ -0,0 +1,54 @@
+﻿using Editor;
+using Sandbox;
+using Sandbox.util;
+using System.Collections.Generic;
+
+namespace Cinema;
+
+[Library("ent_soda_machine"), HammerEntity]
+[Title("Soda Machine"), Category("Gameplay"), Icon("storefront")]
+[EditorModel("models/vendingmachine/vendingmachine.vmdl")]
+public partial class SodaMachine : AnimatedEntity, ICinemaUse
+{
+    [Net] public IDictionary<string, BaseInteractable> Interactables { get; set; }
+
+    /// <summary>
+    /// Set up the model when spawned by the server
+    /// Setup model
+    /// Setup interactions
+    /// </summary>
+    public override void Spawn()
+    {
+        base.Spawn();
+
+        Transmit = TransmitType.Always;
+
+        SetModel("models/vendingmachine/vendingmachine.vmdl");
+
+        SetupPhysicsFromModel(PhysicsMotionType.Keyframed);
+
+        AddInteractables();
+
+        Tags.Add("interactable");
+    }
+
+    /// <summary>
+    /// Adds the interaction volumes from model doc 
+    /// </summary>
+    public void AddInteractables()
+    {
+        Interactables = new Dictionary<string, BaseInteractable>();
+    }
+
+    /// <summary>
+    /// Simulates the interaction volumes on the server
+    /// </summary>
+    [GameEvent.Tick.Server]
+    public void OnServerTick()
+    {
+        foreach (var (_, interactable) in Interactables)
+        {
+            
+        }
+    }
+}
