7295f2ccf08ce64c71883790f647f377e96bbedd
walker407 <hscott407@gmail.com>
2023-07-18 12:38:19 -0500

    Initial commit

---
+++ code/entities/service-bell/ServiceBell.Use.cs
@@ -0,0 +1,41 @@
+﻿using Sandbox;
+
+namespace Cinema;
+
+public partial class ServiceBell
+{
+    public string UseText { get; set; } = "Ring Service Bell";
+
+    /// <summary>
+    /// Whether this entity is usable or not
+    /// </summary>
+    /// <param name="user">The player who is using</param>
+    /// <returns>If this is useable</returns>
+    public virtual bool IsUsable(Entity user)
+    {
+        return true;
+    }
+
+    /// <summary>
+    /// Called on the server when the entity is used by a player
+    /// </summary>
+    /// <param name="user"></param>
+    /// <returns>If the player can continue to use the entity</returns>
+    public virtual bool OnUse(Entity user)
+    {
+        if (Game.IsClient) return false;
+
+        Log.Info("Ringing service bell...");
+
+        return false;
+    }
+
+    /// <summary>
+    /// Called on the server when the entity is stopped being used by a player
+    /// </summary>
+    /// <param name="user"></param>
+    public void OnStopUse(Entity user)
+    {
+
+    }
+}
---
+++ code/entities/service-bell/ServiceBell.cs
@@ -0,0 +1,26 @@
+﻿using Editor;
+using Sandbox;
+
+namespace Cinema;
+
+[Library("ent_service_bell"), HammerEntity]
+[Title("Service Bell"), Category("Gameplay"), Icon("notifications")]
+[EditorModel("models/servicebell/servicebell.vmdl")]
+public partial class ServiceBell : AnimatedEntity, ICinemaUse
+{
+
+    /// <summary>
+    /// Set up the model when spawned by the server
+    /// Setup model
+    /// </summary>
+    public override void Spawn()
+    {
+        base.Spawn();
+
+        Transmit = TransmitType.Always;
+
+        SetModel("models/servicebell/servicebell.vmdl");
+
+        SetupPhysicsFromModel(PhysicsMotionType.Dynamic);
+    }
+}
