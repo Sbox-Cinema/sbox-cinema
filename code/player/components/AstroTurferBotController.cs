6c9ce8410e4beae6a0bd694222afb3996a421154
ducc <spielerc@gmail.com>
2023-07-24 13:09:02 -0600

    Make a controller for easier astroturf debugging

---
+++ code/player/components/AstroTurferBotController.cs
@@ -0,0 +1,36 @@
+﻿using Sandbox;
+
+namespace Cinema;
+
+public partial class AstroTurferBotController : EntityComponent<Player>, ISingletonComponent
+{
+    [ConCmd.Server("bot.astro.debug")]
+    public static void BeginDebug()
+    {
+        if (ConsoleSystem.Caller?.Pawn is not Player ply)
+            return;
+
+        ply.Components.Create<AstroTurferBotController>();
+    }
+    
+    [GameEvent.Client.BuildInput]
+    public void OnBuildInput()
+    {
+        if (Input.Pressed("BotSpawn"))
+        {
+            AstroTurferBot.SpawnAstroTurfer();
+        }
+        if (Input.Pressed("BotQueue"))
+        {
+            AstroTurferBot.QueueTestMedia();
+        }
+        if (Input.Pressed("BotLike"))
+        {
+            AstroTurferBot.LikeCurrentMedia();
+        }
+        if (Input.Pressed("BotDislike"))
+        {
+            AstroTurferBot.DislikeCurrentMedia();
+        }
+    }
+}
