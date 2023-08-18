abf425ac7745b49e2508984f97c68599077f7489
walker407 <hscott407@gmail.com>
2023-08-18 09:47:42 -0500

    Initial commit

---
+++ code/entities/beach-ball/BeachBall.Use.cs
@@ -0,0 +1,37 @@
+﻿using Sandbox;
+
+namespace Cinema;
+
+public partial class BeachBall
+{
+    public string UseText => "Pick up Beach Ball";
+
+    /// <summary>
+    /// Whether this entity is usable or not
+    /// </summary>
+    /// <param name="user">The player who is using</param>
+    /// <returns>If this is useable</returns>
+    public virtual bool IsUsable(Entity user)
+    {
+        return true;
+    }
+
+    /// <summary>
+    /// Called on the server when the entity is used by a player
+    /// </summary>
+    /// <param name="user"></param>
+    /// <returns>If the player can continue to use the entity</returns>
+    public virtual bool OnUse(Entity user)
+    {
+        return false;
+    }
+
+    /// <summary>
+    /// Called on the server when the entity is stopped being used by a player
+    /// </summary>
+    /// <param name="user"></param>
+    public void OnStopUse(Entity user)
+    {
+
+    }
+}
---
+++ code/entities/beach-ball/BeachBall.cs
@@ -0,0 +1,23 @@
+﻿using Editor;
+using Sandbox;
+
+namespace Cinema;
+
+[Library("ent_beach_ball"), HammerEntity]
+[Title("Beach Ball"), Category("Gameplay"), Icon("attractions")]
+[EditorModel("models/citizen_props/beachball.vmdl")]
+public partial class BeachBall : AnimatedEntity, ICinemaUse
+{
+    /// <summary>
+    /// Set up the model when spawned by the server
+    /// Setup model
+    /// </summary>
+    public override void Spawn()
+    {
+        base.Spawn();
+
+        Model = Cloud.Model("garry.beachball");
+
+        SetupPhysicsFromModel(PhysicsMotionType.Dynamic);
+    }
+}
