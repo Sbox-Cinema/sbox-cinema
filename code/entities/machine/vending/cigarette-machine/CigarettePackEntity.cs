9c7abaebca5a8ac41959eb0a910988e0f7ecc7ea
walker407 <hscott407@gmail.com>
2023-08-22 09:50:45 -0500

    Adding CigarettePack Entity/Weapon...

---
+++ code/entities/machine/vending/cigarette-machine/CigarettePackEntity.cs
@@ -0,0 +1,31 @@
+﻿using Sandbox;
+using Conna.Inventory;
+
+namespace Cinema;
+
+public partial class CigarettePackEntity : ModelEntity, IUse
+{
+    public bool IsUsable(Entity user)
+    {
+        return true;
+    }
+
+    public bool OnUse(Entity user)
+    {
+        if(user is Player player)
+        {
+            player.PickupItem(InventorySystem.CreateItem("cigarettepack-strikeforce"));
+        }
+
+        this.Delete();
+
+        return false;
+    }
+
+    public override void Spawn()
+    {
+        base.Spawn();
+
+        SetupPhysicsFromModel(PhysicsMotionType.Dynamic);
+    }
+}
